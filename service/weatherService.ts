186e5cecc524871c035e26bd4cffa9398046d378
Pavithranath <pavimegan77@gmail.com>
2024-04-24 08:07:22 +0530

    add key on env

--- service/weatherService.ts
+++ service/weatherService.ts
@@ -1,12 +1,12 @@
 export const fetchWeatherData = async (city: string) => {
-  const apiKey = "b87e33652d6e9220fb80bbfb24df170a";
+  const apiKey = process.env.API_KEY;
   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
   const response = await fetch(apiUrl);
   const data = await response.json();
   return data;
 };
 export const fetchAirData = async (lat: any, lon: any) => {
-  const apiKey = "b87e33652d6e9220fb80bbfb24df170a";
+  const apiKey = process.env.API_KEY;
   const apiUrl = `http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${apiKey}`;
   const response = await fetch(apiUrl);
   const data = await response.json();
